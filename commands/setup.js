f12c780c6f84f60157c06e898ca91ae5dc527229
Drosscent <kevin.veronesipro@gmail.com>
2022-04-20 21:36:37 +0200

    add: check if the user is in the database

--- commands/setup.js
+++ commands/setup.js
@@ -1,10 +1,13 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { createMember } = require("../utils/utilities");
+const { createMember, checkMember } = require("../utils/utilities");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("setup")
     .setDescription("Débuter votre aventure"),
   async execute(interaction) {
+    if (checkMember(interaction.member)) {
+      return interaction.reply("Vous êtes déjà dans la base de données");
+    }
     createMember(interaction.member);
     return interaction.reply("Bienvenue sur notre jeu!");
   },
